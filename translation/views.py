07f6717dbe8f8ab798e0eb8b4740eed08a945822
Max Blacher-L <m.blacher@web.de>
2020-12-05 23:25:07 +0100

    added admin, updated urls

--- translation/views.py
+++ translation/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
 from django.utils.translation import ugettext as _
 
+
 def index(request):
     message = _("Hello, world. You're at the translation index.")
     return HttpResponse(message)
+
+
+def translation(request, language_id):
+    return HttpResponse("You're translating %s." % language_id)
