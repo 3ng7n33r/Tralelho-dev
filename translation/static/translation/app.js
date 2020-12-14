0b716826fa83f39a9371771c668c5fb0b72571a2
Max Blacher-L <m.blacher@web.de>
2020-12-14 15:49:42 +0100

    Squash of local development branch setup crowdin integration
    populated templates, added css
    
    added favicon
    
    updated index template
    
    Working navbar in mobile view
    
    WIP make translation flags in language work
    
    Navbar mobile and Ipad index page done
    
    Tweaks for non mobile
    
    tweaked again
    
    flags folder, transflag according language_id
    
    added navbar to all pages
    
    added app.js
    
    section headlines are now h2, table is todo
    
    table styling
    
    added sidenavbar for translations
    
    cleaned up js
    
    added docindex
    
    added translation window
    
    added text and Countries table
    
    integrated europe correctly
    
    added flags, updated model
    
    updated flag display style
    
    upsidedown selection works
    
    included locale middleware
    
    translation page works
    
    index page done
    
    Updated navbar everywhere
    
    Added change base button
    
    collapsed langcontainers
    
    added and renamed documents
    
    added documents section

---
+++ translation/static/translation/app.js
@@ -0,0 +1,55 @@
+function collapsenav() {
+	let coll = document.getElementsByClassName("collapsible")[0];
+	coll.classList.toggle("active");
+	let content = document.getElementsByClassName("content")[0];
+    if (content.style.display === "block") {
+		content.style.display = "none";
+	  } else {
+		content.style.display = "block";
+	  }
+}
+
+function txtUpDown() {
+	let coll = document.getElementsByClassName("transbutton")[0];
+	coll.classList.toggle("active");
+	let content = document.getElementsByClassName("txtUpsideDown")[0];
+    if (content.style.display === "block") {
+		content.style.display = "none";
+	  } else {
+		content.style.display = "block";
+	  }
+}
+
+function togglerow(id) {
+	let old = document.getElementsByClassName("activerow");
+	while(old.length > 0){
+		old[0].classList.remove("activerow");
+	}
+
+	let element = document.getElementById(id);
+	element.classList.add("activerow");
+
+	let text = element.getElementsByTagName("p")[1].textContent;
+
+	let updown = document.getElementsByClassName("txtUpsideDown")[0];
+	updown.textContent = text;
+  }
+
+  function switchVisibilityBlock(ID) {
+	let element = document.getElementById(ID);
+    if (element.style.display === "block") {
+		element.style.display = "none";
+	  } else {
+		element.style.display = "block";
+	  }
+  } 
+
+  function switchVisibilityFlex(ID) {
+	let element = document.getElementById(ID);
+    if (element.style.display === "flex") {
+		element.style.display = "none";
+	  } else {
+		/* element.style.className = "langcontainer"; */
+		element.style.display = "flex";
+	  }
+  } 
--- translation/views.py
+++ translation/views.py
@@ -2,11 +2,136 @@ from django.http import HttpResponse
 from django.utils.translation import ugettext as _
 from django.shortcuts import render
 
-
-def index(request):
-    return render(request, 'translation/index.html')
+from .models import Countries
 
 
-def translation(request, language_id):
-    context = {'language_id': language_id}
+def index(request, base_language="fra", base_flag="fra"):
+    afrique = Countries.objects.filter(Continent__iexact="Afrique")
+    namerique = Countries.objects.filter(Continent__iexact="Amerique du Nord")
+    samerique = Countries.objects.filter(Continent__iexact="Amerique du Sud")
+    oceanie = Countries.objects.filter(Continent__iexact="Oceania")
+    asie = Countries.objects.filter(Continent__iexact="Asie")
+    europe = Countries.objects.filter(Continent__iexact="Europe")
+    context = {
+        'afrique': afrique,
+        'namerique': namerique,
+        'samerique': samerique,
+        'oceanie': oceanie,
+        'asie': asie,
+        'europe': europe,
+        'base_language': base_language,
+        'base_flag': base_flag,
+    }
+    return render(request, 'translation/index.html', context)
+
+
+def translation(request, base_language, base_flag, target_language, target_flag):
+    afrique = Countries.objects.filter(Continent__iexact="Afrique")
+    namerique = Countries.objects.filter(Continent__iexact="Amerique du Nord")
+    samerique = Countries.objects.filter(Continent__iexact="Amerique du Sud")
+    oceanie = Countries.objects.filter(Continent__iexact="Oceania")
+    asie = Countries.objects.filter(Continent__iexact="Asie")
+    europe = Countries.objects.filter(Continent__iexact="Europe")
+    context = {
+        'afrique': afrique,
+        'namerique': namerique,
+        'samerique': samerique,
+        'oceanie': oceanie,
+        'asie': asie,
+        'europe': europe,
+        'base_language': base_language,
+        'base_flag': base_flag,
+        'target_language': target_language,
+        'target_flag': target_flag,
+    }
     return render(request, 'translation/language.html', context)
+
+
+def docindex(request, base_language="fra", base_flag="fra"):
+    afrique = Countries.objects.filter(Continent__iexact="Afrique")
+    namerique = Countries.objects.filter(Continent__iexact="Amerique du Nord")
+    samerique = Countries.objects.filter(Continent__iexact="Amerique du Sud")
+    oceanie = Countries.objects.filter(Continent__iexact="Oceania")
+    asie = Countries.objects.filter(Continent__iexact="Asie")
+    europe = Countries.objects.filter(Continent__iexact="Europe")
+    context = {
+        'afrique': afrique,
+        'namerique': namerique,
+        'samerique': samerique,
+        'oceanie': oceanie,
+        'asie': asie,
+        'europe': europe,
+        'base_language': base_language,
+        'base_flag': base_flag,
+    }
+    return render(request, 'translation/docindex.html', context)
+
+
+def documents(request, base_language, base_flag, target_language, target_flag):
+    afrique = Countries.objects.filter(Continent__iexact="Afrique")
+    namerique = Countries.objects.filter(Continent__iexact="Amerique du Nord")
+    samerique = Countries.objects.filter(Continent__iexact="Amerique du Sud")
+    oceanie = Countries.objects.filter(Continent__iexact="Oceania")
+    asie = Countries.objects.filter(Continent__iexact="Asie")
+    europe = Countries.objects.filter(Continent__iexact="Europe")
+    docs = {
+        "Anesthésie": "ane",
+        "IRM": "irm",
+        "Patient": "pat",
+        "Pédiatrie": "ped",
+        "Scanner": "scn",
+        "Secrétariat": "sec"}
+    context = {
+        'afrique': afrique,
+        'namerique': namerique,
+        'samerique': samerique,
+        'oceanie': oceanie,
+        'asie': asie,
+        'europe': europe,
+        'base_language': base_language,
+        'base_flag': base_flag,
+        'target_language': target_language,
+        'target_flag': target_flag,
+        'docs': docs,
+    }
+    return render(request, 'translation/documents.html', context)
+
+
+def support(request, base_language="fra", base_flag="fra"):
+    afrique = Countries.objects.filter(Continent__iexact="Afrique")
+    namerique = Countries.objects.filter(Continent__iexact="Amerique du Nord")
+    samerique = Countries.objects.filter(Continent__iexact="Amerique du Sud")
+    oceanie = Countries.objects.filter(Continent__iexact="Oceania")
+    asie = Countries.objects.filter(Continent__iexact="Asie")
+    europe = Countries.objects.filter(Continent__iexact="Europe")
+    context = {
+        'afrique': afrique,
+        'namerique': namerique,
+        'samerique': samerique,
+        'oceanie': oceanie,
+        'asie': asie,
+        'europe': europe,
+        'base_language': base_language,
+        'base_flag': base_flag,
+    }
+    return render(request, 'translation/support.html', context)
+
+
+def about(request, base_language="fra", base_flag="fra"):
+    afrique = Countries.objects.filter(Continent__iexact="Afrique")
+    namerique = Countries.objects.filter(Continent__iexact="Amerique du Nord")
+    samerique = Countries.objects.filter(Continent__iexact="Amerique du Sud")
+    oceanie = Countries.objects.filter(Continent__iexact="Oceania")
+    asie = Countries.objects.filter(Continent__iexact="Asie")
+    europe = Countries.objects.filter(Continent__iexact="Europe")
+    context = {
+        'afrique': afrique,
+        'namerique': namerique,
+        'samerique': samerique,
+        'oceanie': oceanie,
+        'asie': asie,
+        'europe': europe,
+        'base_language': base_language,
+        'base_flag': base_flag,
+    }
+    return render(request, 'translation/about.html', context)
