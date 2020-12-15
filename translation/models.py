9efb902be08f72d12cf84a945b437dee91fbd5f2
Max Blacher-L <m.blacher@web.de>
2020-12-15 01:08:53 +0100

    populated templates, added css

--- translation/models.py
+++ translation/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+""" class Countries(models.Model):
+    Iso-639 = models.CharField(max_length=3)
+    Name_fr = models.CharField(max_length=50)
+    Name_eng = models.CharField(max_length=50)
+    Continent = models.CharField(max_length=10) """
