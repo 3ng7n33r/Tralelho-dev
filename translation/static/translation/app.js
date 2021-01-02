b9d933cd9b70f65db3d5a927df290746a8b6fa96
Max Blacher-L <m.blacher@web.de>
2021-01-02 21:34:59 +0100

    cleaned up js

--- translation/static/translation/app.js
+++ translation/static/translation/app.js
@@ -1,21 +1,10 @@
-let coll = document.getElementsByClassName("collapsible");
-let i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    let content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-} 
-
 function collapsenav() {
 	let coll = document.getElementsByClassName("collapsible");
 	coll[0].classList.toggle("active");
-	let nav = document.getElementsByClassName("content");
-	nav[0].style.display = "none";
+	let content = document.getElementsByClassName("content");
+    if (content[0].style.display === "block") {
+		content[0].style.display = "none";
+	  } else {
+		content[0].style.display = "block";
+	  }
 }
