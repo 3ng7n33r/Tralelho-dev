0a1a0f602e07af3fdd31498d23984ead767c659d
Max Blacher-L <m.blacher@web.de>
2021-01-04 23:36:26 +0100

    added text and Countries table

--- translation/models.py
+++ translation/models.py
@@ -1,8 +1,16 @@
 from django.db import models
 
 
-""" class Countries(models.Model):
-    Iso-639 = models.CharField(max_length=3)
-    Name_fr = models.CharField(max_length=50)
-    Name_eng = models.CharField(max_length=50)
-    Continent = models.CharField(max_length=10) """
+class Countries(models.Model):
+    """ Iso-639-2/T """
+    code_3d = models.CharField(max_length=3)
+    """ ISO-3166-1-alpha2 """
+    code_2d = models.CharField(max_length=2, unique=True)
+    Name_fr = models.CharField(max_length=50, unique=True)
+    Name_eng = models.CharField(max_length=50, unique=True)
+    Continent = models.CharField(max_length=50)
+    Continent2 = models.CharField(max_length=50, blank=True)
+    Translated = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.Name_eng
