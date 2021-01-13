424c68e1c0552a76cc6507c31e58c9e82dc28e26
Max Blacher-L <m.blacher@web.de>
2021-01-13 13:05:10 +0100

    upsidedown selection works

--- translation/static/translation/app.js
+++ translation/static/translation/app.js
@@ -1,21 +1,36 @@
 function collapsenav() {
-	let coll = document.getElementsByClassName("collapsible");
-	coll[0].classList.toggle("active");
-	let content = document.getElementsByClassName("content");
-    if (content[0].style.display === "block") {
-		content[0].style.display = "none";
+	let coll = document.getElementsByClassName("collapsible")[0];
+	coll.classList.toggle("active");
+	let content = document.getElementsByClassName("content")[0];
+    if (content.style.display === "block") {
+		content.style.display = "none";
 	  } else {
-		content[0].style.display = "block";
+		content.style.display = "block";
 	  }
 }
 
 function txtUpDown() {
-	let coll = document.getElementsByClassName("transbutton");
-	coll[0].classList.toggle("active");
-	let content = document.getElementsByClassName("txtUpsideDown");
-    if (content[0].style.display === "block") {
-		content[0].style.display = "none";
+	let coll = document.getElementsByClassName("transbutton")[0];
+	coll.classList.toggle("active");
+	let content = document.getElementsByClassName("txtUpsideDown")[0];
+    if (content.style.display === "block") {
+		content.style.display = "none";
 	  } else {
-		content[0].style.display = "block";
+		content.style.display = "block";
 	  }
 }
+
+function togglerow(id) {
+	let old = document.getElementsByClassName("activerow");
+	while(old.length > 0){
+		old[0].classList.remove("activerow");
+	}
+
+	let element = document.getElementById(id);
+	element.classList.add("activerow");
+
+	let text = element.getElementsByTagName("p")[1].textContent;
+
+	let updown = document.getElementsByClassName("txtUpsideDown")[0];
+	updown.textContent = text;
+  } 
