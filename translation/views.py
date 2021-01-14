e0e03643958178d4ceeda033c1ddca5c52923354
Max Blacher-L <m.blacher@web.de>
2021-01-14 12:26:24 +0100

    translation page works
    index page done
    
    Updated navbar everywhere
    
    Added change base button
    
    collapsed langcontainers

--- translation/views.py
+++ translation/views.py
@@ -5,7 +5,7 @@ from django.shortcuts import render
 from .models import Countries
 
 
-def index(request):
+def index(request, base_language="fra", base_flag="fra"):
     afrique = Countries.objects.filter(Continent__iexact="Afrique")
     namerique = Countries.objects.filter(Continent__iexact="Amerique du Nord")
     samerique = Countries.objects.filter(Continent__iexact="Amerique du Sud")
@@ -19,12 +19,19 @@ def index(request):
         'oceanie': oceanie,
         'asie': asie,
         'europe': europe,
+        'base_language': base_language,
+        'base_flag': base_flag,
     }
     return render(request, 'translation/index.html', context)
 
 
-def translation(request, language_id):
-    context = {'language_id': language_id}
+def translation(request, base_language, base_flag, target_language, target_flag):
+    context = {
+        'base_language': base_language,
+        'base_flag': base_flag,
+        'target_language': target_language,
+        'target_flag': target_flag,
+    }
     return render(request, 'translation/language.html', context)
 
 
