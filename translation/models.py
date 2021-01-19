d55b91d8cbdc219706b24f282a2a714cec99d394
haraldreingruber <harald.reingruber@gmx.at>
2021-01-19 21:07:34 +0000

    Add Gitpod settings for pytest
    populated templates, added css
    
    added favicon
    
    updated index template
    
    WIP - fix navbar
    
    Working navbar in mobile view
    
    WIP make translation flags in language work
    
    Navbar mobile and Ipad index page done
    
    Tweaks for non mobile
    
    tweaked again
    
    flags folder, transflag according language_id
    
    added navbar to all pages
    
    added app.js
    
    section headlines are now h2, table is todo
    
    table styling
    
    added sidenavbar for translations
    
    cleaned up js
    
    added docindex
    
    added translation window
    
    added text and Countries table
    
    integrated europe correctly
    
    added flags, updated model
    
    updated flag display style
    
    upsidedown selection works
    
    included locale middleware
    
    translation page works
    
    index page done
    
    Updated navbar everywhere
    
    Added change base button
    
    collapsed langcontainers
    
    added and renamed documents
    
    added documents section
    
    fixed header redirection bug

--- translation/models.py
+++ translation/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Countries(models.Model):
+    """ Iso-639-2/T // BCP - 47"""
+    langcode = models.CharField(max_length=3)
+    """ ISO 3166-1 alpha-3 """
+    flagcode = models.CharField(max_length=3)
+    Name_fr = models.CharField(max_length=50, unique=True)
+    Name_eng = models.CharField(max_length=50, unique=True)
+    Continent = models.CharField(max_length=50)
+    Continent2 = models.CharField(max_length=50, blank=True)
+    Translated = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.Name_eng
--- translation/urls.py
+++ translation/urls.py
@@ -4,6 +4,18 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<str:base_language>/<str:base_flag>', views.index, name='index'),
     # ex: /de/
-    path('<str:language_id>/', views.translation, name='translation'),
+    path('<str:base_language>/<str:base_flag>/<str:target_language>/<str:target_flag>',
+         views.translation, name='translation'),
+    path('<str:base_language>/<str:base_flag>/<str:target_language>/<str:target_flag>/documents',
+         views.documents, name='documents'),
+    path('<str:base_language>/<str:base_flag>/documents',
+         views.docindex, name='docindex'),
+    path('support', views.support, name='support'),
+    path('<str:base_language>/<str:base_flag>/support',
+         views.support, name='support'),
+    path('about', views.about, name='about'),
+    path('<str:base_language>/<str:base_flag>/about',
+         views.about, name='about'),
 ]
