b433a18195e2cba298580155a7b4b1f308db0f1e
Max Blacher-L <m.blacher@web.de>
2021-02-07 11:07:21 +0100

    spelling

--- translation/forms.py
+++ translation/forms.py
@@ -7,9 +7,9 @@ from translation.models import Country
 
 class searchcountryform(forms.Form):
     error_class = 'error'
-    countrystring = _("Search for Country")
+    countrystring = _("Rechercher un pays")
     country_name = forms.CharField(label="", max_length=50, widget=forms.TextInput(
-        attrs={'placeholder': countrystring, 'class': 'form-control', 'id': 'txtSearch', 'name': 'txtSearch'}))
+        attrs={'placeholder': countrystring, 'id': 'txtSearch', 'name': 'txtSearch'}))
 
     def clean_country_name(self):
         data = self.cleaned_data['country_name']
@@ -18,5 +18,5 @@ class searchcountryform(forms.Form):
             Country.objects.get(Name_eng__iexact=data)
         except:
             raise ValidationError(
-                _('Country not found. Please check spelling'))
+                _("Nous n'avons pas trouvé ce pays. Merci de verifier l'orthographe."))
         return data
