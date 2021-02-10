9eb0072ddca544a7610a70f27bb0b4c307273a78
Max Blacher-L <m.blacher@web.de>
2021-02-10 15:53:33 +0100

    stashed change

--- translation/forms.py
+++ translation/forms.py
@@ -9,11 +9,7 @@ class searchcountryform(forms.Form):
     error_class = 'error'
     countrystring = _("Rechercher un pays")
     country_name = forms.CharField(label="", max_length=50, widget=forms.TextInput(
-<<<<<<< HEAD
-        attrs={'placeholder': countrystring, 'id': 'txtSearch'}))
-=======
         attrs={'placeholder': countrystring, 'id': 'txtSearch', 'name': 'txtSearch'}))
->>>>>>> ed2612bdf30efc9de4cab7df6e481e366faf87d8
 
     def clean_country_name(self):
         data = self.cleaned_data['country_name']
