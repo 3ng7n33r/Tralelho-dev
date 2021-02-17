f33ae1d3f1e3bec62235df93ee4e2d1b57f3fdf6
Max Blacher-L <m.blacher@web.de>
2021-02-17 17:35:54 +0100

    basic 200 and 404 tests

--- translation/tests.py
+++ translation/tests.py
@@ -4,60 +4,52 @@ from .models import Country, Language
 
 
 class IndexTestCase(TestCase):
-    def setUp(self):
-        Language.objects.create(
-            langcode="eng",
-            Name_fr="Anglais",
-            Name_eng="English",
-            Translated="True",
-        )
-        Language.objects.create(
-            langcode="fra",
-            Name_fr="Fran\u00e7ais",
-            Name_eng="French",
-            Translated="True"
-        )
-        Language.objects.create(
-            langcode="deu",
-            Name_fr="Allemand",
-            Name_eng="German",
-            Translated="True"
-        )
-        Country.objects.create(
-            Name_eng="Germany",
-            Name_fr="Allemagne",
-            countrycode="deu",
-            Continent="EU",
-            spoken_languages=[
-                3
-            ]
-        )
-        Country.objects.create(
-            Name_eng="France",
-            Name_fr="France",
-            countrycode="fra",
-            Continent="EU",
-            spoken_languages=[
-                2
-            ]
-        )
-        Country.objects.create(
-            Name_eng="Switzerland",
-            Name_fr="Suisse",
-            countrycode="che",
-            Continent="EU",
-            spoken_languages=[
-                14,
-                24
-            ]
-        )
-        Country.objects.create(
-            Name_eng="Madagascar",
-            Name_fr="Madagascar",
-            countrycode="mdg",
-            Continent="AF",
-            spoken_languages=[
-                1,
-                2,
-            ]
-        )
+    fixtures = ['languages_test.json', 'countries_test.json']
+
+    def test_translation_success(self):
+        response = self.client.get('/fra/fra/deu/deu')
+        self.assertEqual(response.status_code, 200)
+
+    def test_translation_fail(self):
+        response = self.client.get('/fra/deu/deu/deu')
+        self.assertEqual(response.status_code, 404)
+
+    def test_index_noinput_success(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_index_success(self):
+        response = self.client.get('/fra/fra')
+        self.assertEqual(response.status_code, 200)
+
+    def test_index_fail(self):
+        response = self.client.get('/fra/deu')
+        self.assertEqual(response.status_code, 404)
+
+    def test_docindex_success(self):
+        response = self.client.get('/fra/fra/documents')
+        self.assertEqual(response.status_code, 200)
+
+    def test_docindex_fail(self):
+        response = self.client.get('/fra/deu/documents')
+        self.assertEqual(response.status_code, 404)
+
+    def test_doc_success(self):
+        response = self.client.get('/fra/fra/deu/deu/documents')
+        self.assertEqual(response.status_code, 200)
+
+    def test_doc_fail(self):
+        response = self.client.get('/fra/deu/deu/deu/documents')
+        self.assertEqual(response.status_code, 404)
+
+    def test_disclaimer_success(self):
+        response = self.client.get('/fra/fra/disclaimer')
+        self.assertEqual(response.status_code, 200)
+
+    def test_about_success(self):
+        response = self.client.get('/fra/fra/about')
+        self.assertEqual(response.status_code, 200)
+
+    def test_support_success(self):
+        response = self.client.get('/fra/fra/support')
+        self.assertEqual(response.status_code, 200)
