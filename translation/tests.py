8111d4f0c449614e1aa98caa7cef6eb99cc986b1
Max Blacher-L <m.blacher@web.de>
2021-02-17 14:55:58 +0100

    added 404 functionality

--- translation/tests.py
+++ translation/tests.py
@@ -1,3 +1,63 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Country, Language
+
+
+class IndexTestCase(TestCase):
+    def setUp(self):
+        Language.objects.create(
+            langcode="eng",
+            Name_fr="Anglais",
+            Name_eng="English",
+            Translated="True",
+        )
+        Language.objects.create(
+            langcode="fra",
+            Name_fr="Fran\u00e7ais",
+            Name_eng="French",
+            Translated="True"
+        )
+        Language.objects.create(
+            langcode="deu",
+            Name_fr="Allemand",
+            Name_eng="German",
+            Translated="True"
+        )
+        Country.objects.create(
+            Name_eng="Germany",
+            Name_fr="Allemagne",
+            countrycode="deu",
+            Continent="EU",
+            spoken_languages=[
+                3
+            ]
+        )
+        Country.objects.create(
+            Name_eng="France",
+            Name_fr="France",
+            countrycode="fra",
+            Continent="EU",
+            spoken_languages=[
+                2
+            ]
+        )
+        Country.objects.create(
+            Name_eng="Switzerland",
+            Name_fr="Suisse",
+            countrycode="che",
+            Continent="EU",
+            spoken_languages=[
+                14,
+                24
+            ]
+        )
+        Country.objects.create(
+            Name_eng="Madagascar",
+            Name_fr="Madagascar",
+            countrycode="mdg",
+            Continent="AF",
+            spoken_languages=[
+                1,
+                2,
+            ]
+        )
--- translation/views.py
+++ translation/views.py
@@ -1,5 +1,5 @@
 from django.template.loader import render_to_string
-from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotFound, Http404
 from django.utils.translation import ugettext as _
 from django.shortcuts import render
 from django.core.exceptions import ValidationError
@@ -46,91 +46,107 @@ def autocompleteModel(request, base_language):
 
 
 def index(request, base_language="fra", base_flag="fra"):
-    if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        form = searchcountryform(request.POST)
-        # check whether it's valid:
-        if form.is_valid():
-            Name_form = form.cleaned_data['country_name']
-            countryform = Country.objects.get(Name_eng__iexact=Name_form)
+    if Country.objects.filter(countrycode=base_flag).filter(spoken_languages__langcode=base_language).exists():
+        if request.method == 'POST':
+            # create a form instance and populate it with data from the request:
+            form = searchcountryform(request.POST)
+            # check whether it's valid:
+            if form.is_valid():
+                Name_form = form.cleaned_data['country_name']
+                countryform = Country.objects.get(Name_eng__iexact=Name_form)
 
-            language = countryform.spoken_languages.filter(
-                Translated=True).exclude(langcode=base_language)
-            if len(language) == 1:
-                return HttpResponseRedirect("".join(('/', base_language, '/', base_flag, '/', language.first().langcode, '/', countryform.countrycode)))
-            elif len(language) > 1:
-                return HttpResponseRedirect("".join(('/', base_language, '/', base_flag, '#', countryform.countrycode, 'target', )))
-            else:
-                raise ValidationError(
-                    _("Nous n'avons pas trouvé ce pays. Merci de verifier l'orthographe"))
+                language = countryform.spoken_languages.filter(
+                    Translated=True).exclude(langcode=base_language)
+                if len(language) == 1:
+                    return HttpResponseRedirect("".join(('/', base_language, '/', base_flag, '/', language.first().langcode, '/', countryform.countrycode)))
+                elif len(language) > 1:
+                    return HttpResponseRedirect("".join(('/', base_language, '/', base_flag, '#', countryform.countrycode, 'target', )))
+                else:
+                    raise ValidationError(
+                        _("Nous n'avons pas trouvé ce pays. Merci de verifier l'orthographe"))
 
-    # if a GET (or any other method) we'll create a blank form
+        # if a GET (or any other method) we'll create a blank form
+        else:
+            form = searchcountryform()
+
+        context = {
+            'form': form,
+            'continents': continents,
+            'base_language': base_language,
+            'base_flag': base_flag,
+        }
+        return render(request, 'translation/index.html', context)
     else:
-        form = searchcountryform()
-
-    context = {
-        'form': form,
-        'continents': continents,
-        'base_language': base_language,
-        'base_flag': base_flag,
-    }
-    return render(request, 'translation/index.html', context)
+        raise Http404("No MyModel matches the given query.",
+                      str(base_language))
 
 
 def translation(request, base_language, base_flag, target_language, target_flag):
-
-    context = {
-        'continents': continents,
-        'base_language': base_language,
-        'base_flag': base_flag,
-        'target_language': target_language,
-        'target_flag': target_flag,
-    }
-    return render(request, 'translation/language.html', context)
+    if (Country.objects.filter(countrycode=base_flag).filter(spoken_languages__langcode=base_language).exists() and
+            Country.objects.filter(countrycode=target_flag).filter(spoken_languages__langcode=target_language).exists()):
+        context = {
+            'continents': continents,
+            'base_language': base_language,
+            'base_flag': base_flag,
+            'target_language': target_language,
+            'target_flag': target_flag,
+        }
+        return render(request, 'translation/language.html', context)
+    else:
+        raise Http404("No MyModel matches the given query.",
+                      str(base_language))
 
 
 def docindex(request, base_language="fra", base_flag="fra"):
-    if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        form = searchcountryform(request.POST)
-        # check whether it's valid:
-        if form.is_valid():
-            Name_form = form.cleaned_data['country_name']
-            countryform = Country.objects.get(Name_eng__iexact=Name_form)
+    if Country.objects.filter(countrycode=base_flag).filter(spoken_languages__langcode=base_language).exists():
+        if request.method == 'POST':
+            # create a form instance and populate it with data from the request:
+            form = searchcountryform(request.POST)
+            # check whether it's valid:
+            if form.is_valid():
+                Name_form = form.cleaned_data['country_name']
+                countryform = Country.objects.get(Name_eng__iexact=Name_form)
 
-            language = countryform.spoken_languages.filter(
-                Translated=True).exclude(langcode=base_language)
-            if len(language) == 1:
-                return HttpResponseRedirect("".join(('/', base_language, '/', base_flag, '/', language.first().langcode, '/', countryform.countrycode, '/', 'documents')))
-            else:
-                return HttpResponseRedirect("".join(('/', base_language, '/', base_flag, '/', 'documents', '#', countryform.countrycode, 'target', )))
-        # if a GET (or any other method) we'll create a blank form
+                language = countryform.spoken_languages.filter(
+                    Translated=True).exclude(langcode=base_language)
+                if len(language) == 1:
+                    return HttpResponseRedirect("".join(('/', base_language, '/', base_flag, '/', language.first().langcode, '/', countryform.countrycode, '/', 'documents')))
+                else:
+                    return HttpResponseRedirect("".join(('/', base_language, '/', base_flag, '/', 'documents', '#', countryform.countrycode, 'target', )))
+            # if a GET (or any other method) we'll create a blank form
+        else:
+            form = searchcountryform()
+        context = {
+            'form': form,
+            'continents': continents,
+            'base_language': base_language,
+            'base_flag': base_flag,
+        }
+        return render(request, 'translation/docindex.html', context)
     else:
-        form = searchcountryform()
-    context = {
-        'form': form,
-        'continents': continents,
-        'base_language': base_language,
-        'base_flag': base_flag,
-    }
-    return render(request, 'translation/docindex.html', context)
+        raise Http404("No MyModel matches the given query.",
+                      str(base_language))
 
 
 def documents(request, base_language, base_flag, target_language, target_flag):
-
-    docs = {
-        _("IRM"): "irm",
-        _("Scanner"): "scn",
-    }
-    context = {
-        'continents': continents,
-        'base_language': base_language,
-        'base_flag': base_flag,
-        'target_language': target_language,
-        'target_flag': target_flag,
-        'docs': docs,
-    }
-    return render(request, 'translation/documents.html', context)
+    if (Country.objects.filter(countrycode=base_flag).filter(spoken_languages__langcode=base_language).exists() and
+            Country.objects.filter(countrycode=target_flag).filter(spoken_languages__langcode=target_language).exists()):
+        docs = {
+            _("IRM"): "irm",
+            _("Scanner"): "scn",
+        }
+        context = {
+            'continents': continents,
+            'base_language': base_language,
+            'base_flag': base_flag,
+            'target_language': target_language,
+            'target_flag': target_flag,
+            'docs': docs,
+        }
+        return render(request, 'translation/documents.html', context)
+    else:
+        raise Http404("No MyModel matches the given query.",
+                      str(base_language))
 
 
 '''     docs = {
